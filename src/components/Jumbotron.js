e7d2e0fac13edf715ae84b4ee41f14fa9f9385eb
Dylan Poll <61894413+dylanpoll@users.noreply.github.com>
2020-12-20 15:49:23 -0500

    Add files via upload

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { Jumbotron as Jumbo, Container } from "react-bootstrap";
 import styled from "styled-components";
+import banner from "../assets/creep.png";
 //import greetavi from "../assets/greetme.jpg";
 
 const Styles = styled.div`
   .jumbo {
-    background: #db0000 no-repeat fixed bottom;
-    background-size: cover;
-    color: #efefef;
-    height: 150px;
+    object-fit: contain;
+    background: #e7ad00;
+    max-width: 100%;
+    min-hieght: 1%;
     position: relative;
     z-index: 2;
   }
@@ -22,33 +23,21 @@ const Styles = styled.div`
     right: 0;
     z-index: 3;
   }
-  .greeting {
-    z-index: 4;
-    max-height: 150px;
-    margin-top: 0%;
-    width: auto;
-    height: auto;
-    position: absolute;
-    top: 0;
-    left: 1;
-  }
-  .center {
-    z-index: 4;
-    max-height: 150px;
-    width: auto;
-    height: auto;
-    position: relative;
-    left: 0;
-  }`;
+  .banner {
 
+    z-index: 4;
+    max-height: 100%;
+    max-width: 80%;
+    top: 0;
+    position: absolute;
+  }
+`;
 export const Jumbotron = () => (
   <Styles>
     <Jumbo fluid className="jumbo mb-0">
-      <div classname="overlay"> </div>
+      <div className="overlay"> </div>
       <Container>
-      <div className = "center">
-        <h1>Welcome too CreepCript.RIP</h1>
-      </div>
+      <img src={banner} alt="banner" className = "banner"/>
       </Container>
     </Jumbo>
   </Styles>
--- src/webpages/Home.js
+++ src/webpages/Home.js
@@ -1,60 +1,55 @@
 import { useAuth0 } from '@auth0/auth0-react'
 import React from 'react'
-import { Indent, SmallText, HeaderText, PageTitleText } from "../components/Layout";
+import { Indent, SmallText, HeaderText, PageTitleText} from "../components/Layout";
 
-export const Greeting = () => {
+
+export const Title = () => {
     const { isAuthenticated } = useAuth0();
     return (
         !isAuthenticated && (
-            <PageTitleText>Welcome to CreepCript.RIP</PageTitleText>
+            <PageTitleText>Under Development</PageTitleText>
         )
     )
 }
+
 export const NewGreeting = () => {
     const { user, isAuthenticated } = useAuth0();
     return (
-        isAuthenticated &&(
+        isAuthenticated && (
             <div>
-                <PageTitleText>Welcome Back {user.name}! </PageTitleText>
+                <PageTitleText>Hi {user.name}! </PageTitleText>
             </div>
-            )
+        )
     )
 }
 
 export const Home = () => (
-    <div>
-        <NewGreeting />
-        <Greeting />
+    <div style={{ width: "100%"}}>
+            <PageTitleText>Under Development{'\n'}{'\n'}</PageTitleText>
         <Indent>
-            <HeaderText>About CreepCript.RIP :</HeaderText>
+        <HeaderText>About CreepCript.RIP</HeaderText>
             <Indent>
-                <SmallText>
-                    I have a friend who is and has been stalked online for a long time, this project is meant to help people who are being harrased in that same way.
-                    {'\n'}The project can accomplish :
+                <SmallText>I have a friend who is and has been stalked online for a long time. 
+                    This project is dedicated to that friend and meant to help people who are being harrased in that same way.{'\n'}
+                {'\n'}The project can accomplish :
+                    <ul>
+                        <li><span>Injection of metadata(exif data codes meant to be used for tracking suspect leads)</span></li>
+                        <li><span>Image Stegnography as a means to inject a tracking code to bypass exif data scrubbers</span></li>
+                        <li><span>An easy deploment system ( so you can host a site that will post images that will automatically display tagged images for the user connected.)</span></li>
+                        <li><span>A phone App</span></li>
+                        <li><span>Archival for people to codes used only accessable after authenticating using Auth0.</span></li>
+                        <li><span>And more.(site is under development, but the backend is almost done.)</span></li>
+                    </ul>
                 </SmallText>
-                <Indent>
-                    <SmallText>
-                        * Injection of metadata(exif data codes meant to be used for tracking suspect leads)
-                        {'\n'}* Image Stegnography as a means to inject a tracking code to bypass exif data scrubbers
-                        {'\n'}* An easy deploment system( so you can host a site that will post images that will automatically display tagged images for the user connected.)
-                        {'\n'}* A phone App
-                        {'\n'}* Archival for people to codes used only accessable after authenticating using Auth0.
-                        {'\n'}* And more.(site is under development, but the backend is almost done.)
-                    </SmallText>
-                </Indent>
             </Indent>
+            
         </Indent>
 
-        <Indent>
-            <HeaderText>Contact Me (Developer) :{'\n'}</HeaderText>
-                <Indent>
-                    <SmallText>
-                        E-Mail : <a href="DylanManPoll@gmail.com">DylanManPoll@gmail.com</a>
-                        {'\n'}GitHub : <a href="https://github.com/dylanpoll"> https://github.com/dylanpoll </a>
-                        {'\n'}LinkdIn : <a href="https://www.linkedin.com/in/dylan-poll-4a324a1a2/">https://www.linkedin.com/in/dylan-poll-4a324a1a2/</a>
-                        {'\n'}My Website : <a href="https://devdylan.me">https://devdylan.me</a>
-                    </SmallText>
-                </Indent>
-        </Indent>
     </div>
 );
+/*
+                    <ul>
+                        <li><span>    </span></li>
+                    </ul>
+*/
+
